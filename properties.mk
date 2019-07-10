9e23f4a99aac8e14801c3ccc1bce80271064719a
Pawin Vongmasa <pawin@google.com>
2019-07-10 04:25:17 -0700

    hanoip: Move input surface to CCodec
    Test: atest CtsMediaTestCases -- \
    --module-arg CtsMediaTestCases:size:small
    Bug: 131800183
    Change-Id: I8783a7de4a4fdf7ff2eac6697c71f6592c9a9e06

--- properties.mk
+++ properties.mk
@@ -262,6 +262,7 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     media.settings.xml=/vendor/etc/media_profiles_vendor.xml \
     media.stagefright.thumbnail.prefer_hw_codecs=true \
+    debug.stagefright.c2inputsurface=-1 \
     persist.vendor.media.recorder.bt709=true
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
