441c951b8a3f52d82cfe00e719c81e60511f1957
Benergy Meenan Ravuri <bravuri@codeaurora.org>
2021-07-12 23:27:12 +0530

    hanoip: gpt-utils: Do fsync after writing partition entries
    Do fsync after writing partition entries to ensure data
    is actually written before rebooting device.
    
    Change-Id: I09659cab247fe280dc608e30a88d912034cbc01a

--- gpt-utils/gpt-utils.cpp
+++ gpt-utils/gpt-utils.cpp
@@ -1537,7 +1537,7 @@ int gpt_disk_commit(struct gpt_disk *disk)
                 ALOGE("%s: Invalid args", __func__);
                 goto error;
         }
-        fd = open(disk->devpath, O_RDWR);
+        fd = open(disk->devpath, O_RDWR | O_DSYNC);
         if (fd < 0) {
                 ALOGE("%s: Failed to open %s: %s",
                                 __func__,
@@ -1571,6 +1571,7 @@ int gpt_disk_commit(struct gpt_disk *disk)
                                 __func__);
                 goto error;
         }
+        fsync(fd);
         close(fd);
         return 0;
 error:
