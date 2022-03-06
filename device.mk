d9702b0f500048b214a23045dcf23bbe4d8aa4bb
Alexander Koskovich <akoskovich@pm.me>
2022-03-06 02:11:47 -0700

    hanoip: Update to Boot Control HAL v1.2
    Change-Id: Ib5d06c9b0c7e2ed000eb03a9bf18e4d78cc8740d

--- device.mk
+++ device.mk
@@ -179,9 +179,9 @@ PRODUCT_PRODUCT_PROPERTIES += \
 
 # Boot control
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.1-impl-qti \
-    android.hardware.boot@1.1-service \
-    android.hardware.boot@1.1-impl-qti.recovery
+    android.hardware.boot@1.2-impl-qti \
+    android.hardware.boot@1.2-service \
+    android.hardware.boot@1.2-impl-qti.recovery
 
 PRODUCT_PACKAGES_DEBUG += \
     bootctl
