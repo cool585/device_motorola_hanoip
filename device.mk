c1d68b8c293847dc02bbaa1cdbbf1d283210f1b2
DhineshCool <dhineshcool585@gmail.com>
2024-10-18 11:40:33 +0200

    hanoip:update everything for los
    Change-Id: I436bbf425023dbe54cf3e2c35f50a5575511bc38

--- device.mk
+++ device.mk
@@ -35,14 +35,11 @@ PRODUCT_PACKAGES += \
     TelephonyResHanoip
 
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage \
-    $(LOCAL_PATH)/overlay-lineage-fastbootd
+    $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage/lineage-sdk
-
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 
 # A/B
 AB_OTA_UPDATER := true
