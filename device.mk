2c0bd090c9b8a0bfae905a46638dfe70c21bf45d
Bruno Martins <bgcngm@gmail.com>
2025-02-06 21:32:31 +0000

    hanoip: Use aidl sensors multihal
    Change-Id: I21da3da39e9726dca693d6bcbe42e93be861f7c7

--- device.mk
+++ device.mk
@@ -405,7 +405,7 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.0-service.multihal
+    android.hardware.sensors-service.multihal
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
