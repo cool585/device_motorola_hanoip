7db5bced32343ad1b160f108d5ce7a7e127b3225
Cosmin Tanislav <demonsingur@gmail.com>
2025-02-19 02:44:05 +0200

    hanoip: switch to common QCOM AIDL bootctrl HAL
    Change-Id: I04ab771d3b1c38b58913607fbff1bb3b55e1fe25

--- device.mk
+++ device.mk
@@ -158,12 +158,8 @@ PRODUCT_PRODUCT_PROPERTIES += \
 
 # Boot control
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.2-impl-qti \
-    android.hardware.boot@1.2-service \
-    android.hardware.boot@1.2-impl-qti.recovery
-
-PRODUCT_PACKAGES_DEBUG += \
-    bootctl
+    android.hardware.boot-service.qti \
+    android.hardware.boot-service.qti.recovery
 
 $(call soong_config_set,QTI_GPT_UTILS,USE_BSG_FRAMEWORK,false)
 
