2e6a5edf38e60163f0ab3a09b81e51d655f3d621
Giovanni Ricca <giovanniricca@proton.me>
2025-03-08 23:02:02 +0100

    hanoip: Drop mediacodec-seccomp.policy
    No longer used since we switched to C2
    
    Change-Id: I1e4891628e382cdd48ded7022d0f47bc09934a0f

--- device.mk
+++ device.mk
@@ -398,10 +398,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vndservicemanager
 
-# Seccomp policy
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
-
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors-service.multihal
