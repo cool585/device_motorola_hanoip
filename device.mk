c7f243d2b382cc85a86e66eb6a32df6c24c2f87e
basamaryan <basam.aryan@gmail.com>
2025-03-08 16:48:11 -0500

    hanoip: Unset VNDK flags
    Change-Id: I2ef63b9e95b35e23b6026b3f28bbb70590b427c1

--- device.mk
+++ device.mk
@@ -445,10 +445,6 @@ PRODUCT_PACKAGES_DEBUG += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service.dual_role_usb
 
-# VNDK
-PRODUCT_ENFORCE_ARTIFACT_PATH_REQUIREMENTS := strict
-PRODUCT_PRODUCT_VNDK_VERSION := current
-
 # Wifi
 PRODUCT_PACKAGES += \
     android.hardware.wifi-service \
