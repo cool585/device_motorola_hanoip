0a61f2181f61e1fd3976b9cf9ea35cf915a1ff73
Michael Bestas <mkbestas@lineageos.org>
2025-03-11 02:17:16 +0200

    hanoip: Add hardware/motorola to soong namespaces
    Change-Id: I1606d2dc9cef82a4ce9d8764c1f52b59d183e9b2

--- device.mk
+++ device.mk
@@ -405,6 +405,7 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/google/interfaces \
     hardware/google/pixel \
     hardware/lineage/interfaces/power-libperfmgr \
+    hardware/motorola \
     hardware/qcom-caf/common/libqti-perfd-client
 
 # Speed profile services and wifi-service to reduce RAM and storage
