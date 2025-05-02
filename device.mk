6fbfbb66b02b765097025fc1f14c52e1ed51e612
DhineshCool <dhineshcool585@gmail.com>
2025-05-02 08:51:48 +0530

    hanoip: import location and gps from moto sm7250-common
    -https://github.com/crdroidandroid/android_device_motorola_sm7250-common
    -https://github.com/LineageOS/android_device_motorola_sm7250-common

--- device.mk
+++ device.mk
@@ -246,9 +246,7 @@ PRODUCT_PACKAGES += \
     android.hardware.gnss@2.1-service-qti \
     libbatching \
     libgeofencing \
-    libgnss \
-    libgnsspps \
-    libsynergy_loc_api
+    libgnss
 
 PRODUCT_PACKAGES += \
     apdr.conf \
