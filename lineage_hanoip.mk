d2c878b38fa79d999f23e19d0f4c789739b4c5c3
DhineshCool <dhineshcool585@gmail.com>
2025-05-14 09:12:53 +0530

    build without gms

--- lineage_hanoip.mk
+++ lineage_hanoip.mk
@@ -26,10 +26,10 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # extras
 # gms
-$(call inherit-product, vendor/gms/gms_mini.mk)
-WITH_GMS := true
+# $(call inherit-product, vendor/gms/gms_mini.mk)
+#WITH_GMS := true
 # Blur
-TARGET_ENABLE_BLUR := true
+# TARGET_ENABLE_BLUR := true
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_hanoip
