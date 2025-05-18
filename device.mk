49ae4ca57eb1d3fd53d6153f3ee8be5e2163ad31
DhineshCool <dhineshcool585@gmail.com>
2025-05-18 22:40:37 +0530

    hanoip:Switch back to passthrough bootctrl HAL

--- device.mk
+++ device.mk
@@ -156,10 +156,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.config.art_lowmem=true
 
-# Boot control
 PRODUCT_PACKAGES += \
-    android.hardware.boot-service.qti \
-    android.hardware.boot-service.qti.recovery
+    android.hardware.boot@1.2-impl-qti \
+    android.hardware.boot@1.2-service \
+    android.hardware.boot@1.2-impl-qti.recovery
 
 $(call soong_config_set,QTI_GPT_UTILS,USE_BSG_FRAMEWORK,false)
 
